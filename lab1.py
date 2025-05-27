d474a172248bbace83fc0a9bf93db85811772597
nevsetakpohano <aya101318@gmail.com>
2025-05-27 16:13:41 +0300

    Added interactive mode with input validation

--- lab1.py
+++ lab1.py
@@ -1,16 +1,34 @@
 import math
 
+def get_valid_number(prompt):
+    while True:
+        try:
+            value = input(prompt).strip()
+            num = float(value)
+            return num
+        except ValueError:
+            print(f"Error. Expected a valid real number, got '{value}' instead")
+
 def solve_quadratic(a, b, c):
     discriminant = b**2 - 4*a*c
     
     if discriminant > 0:
         x1 = (-b + math.sqrt(discriminant)) / (2*a)
         x2 = (-b - math.sqrt(discriminant)) / (2*a)
-        return [x1, x2]
+        return [round(x1, 2), round(x2, 2)]
     elif discriminant == 0:
         x = -b / (2*a)
-        return [x]
+        return [round(x, 2)]
     else:
         return []
 
-print(solve_quadratic(1, -3, 2))
+def interactive_mode():
+    print("Interactive mode. Enter coefficients:")
+    a = get_valid_number("a = ")
+    b = get_valid_number("b = ")
+    c = get_valid_number("c = ")
+    
+    solutions = solve_quadratic(a, b, c)
+    print("Solutions:", solutions)
+
+interactive_mode()
